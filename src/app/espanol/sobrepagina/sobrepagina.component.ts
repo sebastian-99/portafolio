608008ab66ce2c555ee1c09f9f6fa9e43c7f42f9
Sebastián Aguilar <al221810693@gmail.com>
2022-02-26 18:05:38 -0600

    componente acerca listo

--- src/app/espanol/sobrepagina/sobrepagina.component.ts
+++ src/app/espanol/sobrepagina/sobrepagina.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobrepagina',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobrepaginaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:  ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.fragment.subscribe(params =>{
+      const element = document.querySelector("#" + params);
+      if(element) element.scrollIntoView();
+    });
   }
 
 }
